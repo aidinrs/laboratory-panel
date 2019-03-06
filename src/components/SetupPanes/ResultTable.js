d976143eff396aad9af5d19d38f044944e000f47
Aidin <aidin.rasti@gmail.com>
2019-03-06 17:02:32 +0330

    add word wrap in css for option pair component

--- src/components/SetupPanes/ResultTable.js
+++ src/components/SetupPanes/ResultTable.js
@@ -16,7 +16,7 @@ export class ResultTable extends React.Component {
               return <OptionsTablePair label={k.name} key={i}>
                 <ul>
                   {k.fields.map((field) => {
-                    console.log(`${k.name}.[${i}].${field}`)
+                    // console.log(`${k.name}.[${i}].${field}`)
                     let v = _.get(data, `${k.name}.[${i}].${field}`)
                     // return v !== undefined && v !== null && <li key={`${i}.${field}`}>{field}: {v}</li>
                     return v !== undefined && v !== null && <OptionsTablePair label={field} key={`${i}.${field}`}>{v}</OptionsTablePair>
